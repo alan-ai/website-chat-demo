8cc1bbec9803dc89a6594dc8d7631a6e08825917
Olga Kolyachko <olga.kolyachko@gmail.com>
2023-04-11 17:48:39 +0400

    README, scripts added, key updated

---
+++ README.md
@@ -0,0 +1,63 @@
+# Alan AI: Voice and Text AI Chat for Web 
+
+[Alan Platform](https://alan.app/) • [Alan Studio](https://studio.alan.app/register) • [Docs](https://alan.app/docs)
+
+This sample project demonstrates how to quickly add a voice and text AI chat to your web page, website or web app.
+
+## Features
+
+- Conversational AI powered by LLMs and generative AI
+- Automatic question answering
+- Actionable AI: performing actions with voice and text commands
+- Voice and text input modes
+- Customizable chat interface
+
+## Supported web frameworks
+
+* React
+* Angular
+* Vue
+* Ember
+* Electron 
+* JavaScript
+
+## How to start
+
+To get started with the project:
+
+1. Clone the repository to your local machine: `https://github.com/alan-ai/website-chat-demo`. 
+2. Open the `index.html` page in a browser.
+3. In the bottom right corner of the page, click the chat button and type the following commands in the chat:
+
+  * `Who are you?`
+  * `What is your experience with AI?`
+  * `Do you have a portfolio?`
+  * `Show iOS apps/Show all apps`
+  * `Open a resume`
+  
+## Dialog script
+
+To view the dialog script used in this project, do either of the following:
+
+- View the dialog scripts in the `dialog scripts` folder
+- <a href="https://studio.alan.app/register" target="_blank">Sign up for Alan Studio</a> and select **Create project** > **Web Chat Example**
+
+
+## Folder structure
+
+The project is structured in the following way:
+
+* `index.html`: web page with the web chat added
+* `dialog-scripts`: dialog scripts for the web chat conversational experience
+* `assets`: content-related assets (images, PDFs, JS files and so on)
+
+## Live demo
+
+Check out the live web chat demo here: [https://alan-ai.github.io/website-chat-demo/](https://alan-ai.github.io/website-chat-demo/)
+
+
+## Have questions?
+
+If you have any questions or something is missing in the documentation:
+- Join [Alan AI Slack community](https://app.slack.com/client/TL55N530A) for support
+- Contact us at [support@alan.app](mailto:support@alan.app)
---
+++ dialog-scripts/Corpus.js
@@ -0,0 +1,42 @@
+// Web page corpus
+corpus(
+    {url: "https://alan-ai.github.io/website-chat-demo/", depth: 1},
+);
+
+// Text corpus
+corpus(`
+    My name is Alan Turing Jr.
+    I am a highly skilled software developer with expertise in machine learning and conversational AI.
+    I have a strong mathematical background.
+    I have a Master's degree in Computer Science and extensive experience in deep learning.
+    I have spent the past several years specializing in the development of conversational AI systems.
+    I possess strong proficiency in machine learning and mathematical modeling.
+    I am also skilled in NLP techniques and tools.
+    I am highly proficient in Python.
+    I have worked with a reach set of libraries and tools such as TensorFlow, Keras, PyTorch, and scikit-learn.
+    I have some experience in R that I used for statistical analysis and data visualization.
+    Here is a collection of R libraries I have used: ggplot2, caret, and mlr.
+    I have the following Java libraries to develop machine learning models: Weka, Deeplearning4j, and Apache Mahout.
+    I have some experience with C and C++.
+    I have a strong understanding of various neural network architectures, including convolutional neural networks (CNNs), recurrent neural networks (RNNs), and transformer models.
+    I have demonstrated my expertise in designing, developing, launching, and maintaining chatbots, voice assistants, and conversational AI applications.
+    I have created numerous predictive models, including ML-based recommendation systems for various applications. 
+    I have designed and implemented ML models for large datasets.
+    I have created and launched speech-to-text and text-to-speech systems, ML algorithms for classification and clustering, and ML models for fraud detection. 
+    I have worked extensively with popular deep learning libraries.
+    Previously, I worked with various chatbot platforms, such as Alan AI. 
+    I have experience in data processing, including data cleaning, feature engineering, and data augmentation.
+    In my prior roles, I was responsible for the design and implementation of complicated NLU systems for numerous chatbot and AI assistant projects. 
+    I developed and deployed conversational AI applications on various platforms, including iOS, Android, web, and cross-platform solutions such as Ionic, Flutter, and React Native.
+    I have developed models for time-series analysis and forecasting. 
+    I have worked on sentiment analysis and opinion mining projects, created speech recognition models for a variety of languages.
+    I utilized different chatbot analytics tools to improve user experience. 
+    My data analysis and visualization skills are exceptional. 
+    I have some experience developing mathematical models for financial forecasting.
+    Throughout my career, I have worked with various cloud-based conversational AI platforms.
+    I am skilled in utilizing cloud computing platforms, such as AWS and GCP.
+    I am an expert in natural language generating systems design and development. 
+    At my former company, I mainly focused on developing intelligent chatbots for customer support and rule-based AI assistants. 
+    I am also experienced in working with various data management systems for conversational AI.
+    I have experience collaborating with cross-functional teams, including data scientists, software developers, and business stakeholders, to deliver high-quality products.
+`)
